892d273920e2c4d580317e7189c45e06e73fec79
Solomon Shortland <sol.shortland@gmail.com>
2025-10-17 19:25:42 +0100

    auth reset-password page added, using environment variables to find backend

--- frontend-next/src/app/api/getTickers/route.ts
+++ frontend-next/src/app/api/getTickers/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/serverClient";
-import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
   try {
